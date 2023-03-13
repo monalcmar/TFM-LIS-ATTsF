bf3f4338f8057a9f03a8201794f989d76fd69db9
Sergio Retuerto <sergio.retuerto@lisdatasolutions.com>
2023-03-13 12:43:24 +0100

    añadidos mas log

--- app/log_example.py
+++ app/log_example.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import dependencies as dp
 
 
@@ -6,7 +7,7 @@ dp.logger.info('Iniciamos el proceso')
 
 path_input = dp.rootFolder / 'data' / 'raw'
 
-try:
+try:    
     df_camiones = pd.read_excel(
         path_input / 'BASE DE DATO 2022 PRUEBA.xlsx',
         sheet_name='Lista de camiones',
@@ -14,8 +15,26 @@ try:
     )
     dp.logger.info('Leido el fichero BASE DE DATOS 2022 PRUEBA.xlsx')
 
+except FileNotFoundError as e:
+    dp.logger.error(f'Fichero no encontrado: {e}')
+    try:
+        dp.logger.info('Generando otro df porque el otro no se encuentra')
+        df = pd.DataFrame(
+            np.array(
+                [
+                    [1, 2, 3],
+                    [4, 5, 6],
+                    [7, 8, 9]
+                ]
+            ),
+            columnas=['a', 'b', 'c']
+        )
+    except TypeError as e:
+        dp.logger.error(f'Hay un error en los parámetros de lectura: {e}')
+    except Exception as e:
+        dp.logger.error(e)
+
+except ImportError as e:
+    dp.logger.error(f'Faltan dependencias: {e}')
 except Exception as e:
-        dp.logger.exception(e, exc_info=True)
-
-
-
+   dp.logger.error(e)
