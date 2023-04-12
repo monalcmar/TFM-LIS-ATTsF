815291c824dc3f3641e7cccb401bb813de01a0b5
Sergio Retuerto <sergio.retuerto@lisdatasolutions.com>
2023-04-12 11:40:53 +0200

    borrar ficheros

--- app/consultas_sql.sql
+++
@@ -1,6 +0,0 @@
-select date_part('year', salida_fecha_hora), sum(tm) tm_totales 
-from tbl_distribucion 
-where salida_fecha_hora between '2021-01-01 00:00' and '2021-12-31 23:59'
-group by date_part('year', salida_fecha_hora); 
-
-
