9e5729217dd2106cf4e7349eedeca144316c386c
Monica Alcantar Martinez <monica.alcantar@lisdatasolutions.com>
2023-05-02 13:19:54 +0200

    actualizar engine

--- app/db/connection.py
+++ app/db/connection.py
@@ -5,7 +5,7 @@ from sqlalchemy import MetaData
 import dependencies as dp
 
 
-def engine_create(db_type, db_name, user, password, host, port, schema=None):
+def engine_create(db_type, db_name, user, password, host, port, schema=None, setinputsizes=True):  #
     if db_type == 'postgresql':
         engine = create_engine(
             f'postgresql://{user}:{password}@{host}:{port}/{db_name}', connect_args=schema
@@ -13,7 +13,7 @@ def engine_create(db_type, db_name, user, password, host, port, schema=None):
     elif db_type == 'mssql':
         engine = create_engine(
             f'mssql+pyodbc://{user}:{password}@{host}/{db_name}'
-            f'?driver=ODBC+Driver+17+for+SQL+Server'
+            f'?driver=SQL+Server', use_setinputsizes=setinputsizes  #
         )
     elif db_type == 'oracle':
         engine = create_engine(
@@ -25,12 +25,12 @@ def engine_create(db_type, db_name, user, password, host, port, schema=None):
 
 
 engine = engine_create(
-    db_type='postgresql',
+    db_type='mssql',  #
     db_name=dp.config[dp.enviroment]['database']['db'],
     user=os.environ.get('DATABASE_USER'),
     password=os.environ.get('DATABASE_PASSWORD'),
     host=dp.config[dp.enviroment]['database']['host'],
-    port=5432,
-    schema={'options': '-csearch_path=attsf'}
+    port=1433,
+    setinputsizes=False
 )
 conn = engine.connect()
