72b4506197b4cf6a3656c586712cc1b5fea05633
Sergio Retuerto <sergio.retuerto@lisdatasolutions.com>
2023-05-03 14:03:26 +0200

    incluir funciones de mestros en functions

--- app/utils/functions.py
+++ app/utils/functions.py
@@ -49,3 +49,82 @@ def busqueda_hoja(file_name):
             hoja_1 = hoja
 
     return (hoja_1)
+
+
+def elements_not_in_list(list1, list2):
+    """
+    Comprueba que los elementos de la lista 2 están en la lista 1. Si hay alguno
+    que no está, lo retorna.
+
+    Args:
+    - list1: lista de elementos.
+    - list2: lista de elementos a comprobar.
+
+    Returns:
+    - not_in_list1: lista de elementos que no se encuentran en la lista 1.
+    """
+    not_in_list1 = [elem for elem in list2 if elem not in list1]
+
+    if len(not_in_list1) != 0:
+        return not_in_list1
+
+
+def check_relations(db_table, df, df_master, column):
+    """
+    Registra los elementos que tienen algún id de otra tabla mal referenciado.
+
+    Args:
+    - db_table: nombre de la tabla en la que se están comprobando las relaciones.
+    - df: DataFrame de la tabla.
+    - df_master: DataFrame de la tabla maestra.
+    - column: columna que contiene los ids de la tabla maestra.
+
+    Returns:
+    - DataFrame filtrado sin los elementos mal referenciados.
+    """
+    out_elemnts = elements_not_in_list(
+        df_master.iloc[:, 0].tolist(), df[column].tolist()
+    )
+    if out_elemnts is not None:
+        df_filtered = df[df[column].isin(out_elemnts)]
+        dp.logger.warning(
+            f"En la tabla {db_table}, {column} mal referenciado para los siguientes elementos: "
+            + ", ".join(str(id) for id in df_filtered.iloc[:, 1])
+        )
+        return df[~df[column].isin(out_elemnts)]
+    return df
+
+
+def update_db_table(dict_df, Table, table_name, session):
+    """
+    Actualiza la tabla en la base de datos.
+
+    Args:
+    - dict: diccionario con los elementos a actualizar.
+    - Table: tabla de la base de datos.
+    - table_name: nombre de la tabla a actualizar.
+    - session: sesión de la base de datos.
+    """
+    try:
+        for row in dict_df:
+            rec = Table(**row)
+            session.merge(rec)
+        session.commit()
+        dp.logger.info(f'Actualización tabla {table_name}')
+    except Exception as e:
+        dp.logger.error("Ocurrió un error:", e)
+        session.rollback()
+        pass
+
+
+def capitalize_df(df):
+    """
+    Capitaliza todas las columnas de un DataFrame que sean de tipo string.
+
+    Args:
+    - df: DataFrame de Pandas a capitalizar.
+
+    Returns:
+    - DataFrame capitalizado.
+    """
+    return df.applymap(lambda x: x.capitalize() if isinstance(x, str) else x)
