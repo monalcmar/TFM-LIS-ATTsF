bfb5569131e98663a689f350f8d8d3f00a08105a
Monica Alcantar Martinez <monica.alcantar@lisdatasolutions.com>
2023-05-04 14:04:36 +0200

    actualizacion main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from src.ot.main_ot import etl_ot
 from src.maestros.etl_maestos import etl_maestros
+from src.maestros.etl_distribucion import etl_distribucion
 
 # etl maestros
 etl_maestros()
 
 # etl ot
 etl_ot()
+
+# etl distribucion
+etl_distribucion()
